669a81680707e2634fefc687960bddd295b7a93f
chang.jiaming@macbookpro <chang.jiaming@gmail.com>
2021-06-26 19:01:41 +0800

    fix naming incosistenct in genBinMap.cpp

--- src/genBinMap.cpp
+++ src/genBinMap.cpp
@@ -12,19 +12,19 @@
 
 using namespace std;
 
-void parse_command_line(int argc, char **argv, char *in_ncontact_name, char *out_bimap_name);
-void show_param(char *in_ncontact_name, char *out_bimap_name);
+void parse_command_line(int argc, char **argv, char *in_ncontact_name, char *out_binmap_name);
+void show_param(char *in_ncontact_name, char *out_binmap_name);
 
 void exit_with_help()
 {
 	printf(
-		"Turn .n_contact into binary format, .bimap\n"	
-		"Usage: genBiMap [options] -in_ncontact input.n_contact -out_bimap out.bimap\n"
+		"Turn .n_contact into binary format, .binmap\n"	
+		"Usage: genBinMap [options] -in_ncontact input.n_contact -out_binmap out.binmap\n"
 		"options:\n"
 		"\t-in_ncontact \t HiC contact map in .n_contact format\n"
-		"\t-out_bimap \t HiC contact in binary format .bimap\n"
+		"\t-out_binmap \t HiC contact in binary format .binmap\n"
 		"\nFor instance:\n"
-		"genBiMap -in_ncontact example/cm_uniform_1000.n_contact -out_bimap example/1000.bimap\n"
+		"genBinMap -in_ncontact example/cm_uniform_1000.n_contact -out_binmap example/1000.binmap\n"
 	);
 	exit(0);
 }
@@ -32,23 +32,23 @@ void exit_with_help()
 int main(int argc, char *argv[])
 {
 	char in_ncontact_name[1024];
-	char out_bimap_name[1024];
+	char out_binmap_name[1024];
 	
-	parse_command_line(argc, argv, in_ncontact_name, out_bimap_name);
-	show_param(in_ncontact_name, out_bimap_name);
+	parse_command_line(argc, argv, in_ncontact_name, out_binmap_name);
+	show_param(in_ncontact_name, out_binmap_name);
 
 	cout << endl << "[BEGIN]" << endl;
-	BINMAP map(in_ncontact_name, out_bimap_name);
+	BINMAP map(in_ncontact_name, out_binmap_name);
 
 #ifdef DEBUG	
-	BINMAP debug_map(out_bimap_name);
+	BINMAP debug_map(out_binmap_name);
 #endif
 	
 	cout << endl << "[END]" << endl;
 	return 0;
 }
 
-void parse_command_line(int argc, char **argv, char *in_ncontact_name, char *out_bimap_name)
+void parse_command_line(int argc, char **argv, char *in_ncontact_name, char *out_binmap_name)
 {
 	int i;
 	// default values
@@ -67,8 +67,8 @@ void parse_command_line(int argc, char **argv, char *in_ncontact_name, char *out
 		if( strncmp(argv[i-1],"-in_ncontact", 20)==0 ){
 			strcpy(in_ncontact_name, argv[i]);
 		}
-		else if( strncmp(argv[i-1],"-out_bimap", 20)==0 ){
-			strcpy(out_bimap_name, argv[i]);
+		else if( strncmp(argv[i-1],"-out_binmap", 20)==0 ){
+			strcpy(out_binmap_name, argv[i]);
 		}
 		else{
 				fprintf(stderr,"unknown option:%s\n",argv[i-1]);
@@ -81,10 +81,10 @@ void parse_command_line(int argc, char **argv, char *in_ncontact_name, char *out
 		exit_with_help();
 }
 
-void show_param(char *in_ncontact_name, char *out_bimap_name)
+void show_param(char *in_ncontact_name, char *out_binmap_name)
 {
 	cout << "Input" << endl
 	 << "\t n_contact =\t"  << in_ncontact_name << endl
      << "Output binary" << endl
-	 << "\t bimap =\t"  << out_bimap_name << endl;
+	 << "\t binmap =\t"  << out_binmap_name << endl;
 }
